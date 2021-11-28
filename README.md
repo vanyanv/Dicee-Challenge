257965b1fb7e9de4c8fa26d3fdc2f2bcd5f8a36c
Vardan <vardan.vanyan@gmail.com>
2021-11-27 16:43:37 -0800

    made chnages to README, added js code to determine winner and chnage h1 text based on who won.

--- README.md
+++ README.md
@@ -46,8 +46,10 @@ Then crop/optimize/edit your image however you like, add it to your project, and
       ![Random Numbers](/images/randomNumber.PNG)
 - [x] Change the `<img>` to a random dice
       ![Random Numbers](/images/p1ImgChange.PNG)
-- [ ] Change both image elements
+- [x] Change both image elements
+      ![Random Numbers](/images/p2ImgChange.PNG)
 - [ ] Change the title so it displays a winner
+      ![Random Numbers](/images/determiningWinner.PNG)
 
 ### Built with
 
@@ -113,7 +115,7 @@ If you want more help with writing markdown, we'd recommend checking out [The Ma
 
 ### Continued development
 
-Use this section to outline areas that you want to continue focusing on in future projects. These could be concepts you're still not completely comfortable with or techniques you found useful that you want to refine and perfect.
+- [ ]Add functions for code that is being repeated.
 
 **Note: Delete this note and the content within this section and replace with your own plans for continued development.**
 
--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 //variable that generates a random number between 1 and 6
 const max = 6;
 const min = 1;
+
+//random number for player 1
 const randomNumber1 = Math.floor(Math.random() * (max - min + 1) + min);
+//random number for player 2
+const randomNumber2 = Math.floor(Math.random() * (max - min + 1) + min);
+
 console.log(randomNumber1);
+console.log(randomNumber2);
 
 //using dom manipulation to set an attribute
 //grab the what we need to alter
 player1 = document.querySelector(".img1");
+player2 = document.querySelector(".img2");
 
-//use if/else to change the image based on random number
+//use if/else to change the image based on random number for player1
 if (randomNumber1 === 1) {
   player1.setAttribute("src", "images/dice1.png");
 } else if (randomNumber1 === 2) {
@@ -21,4 +28,34 @@ if (randomNumber1 === 1) {
   player1.setAttribute("src", "images/dice5.png");
 } else {
   player1.setAttribute("src", "images/dice6.png");
+}
+
+//use if/else to change the image based on random number for player2
+if (randomNumber2 === 1) {
+  player2.setAttribute("src", "images/dice1.png");
+} else if (randomNumber2 === 2) {
+  player2.setAttribute("src", "images/dice2.png");
+} else if (randomNumber2 === 3) {
+  player2.setAttribute("src", "images/dice3.png");
+} else if (randomNumber2 === 4) {
+  player2.setAttribute("src", "images/dice4.png");
+} else if (randomNumber2 === 5) {
+  player2.setAttribute("src", "images/dice5.png");
+} else {
+  player2.setAttribute("src", "images/dice6.png");
+}
+
+//select the h1 element and set it as a variable
+let h1 = document.querySelector("h1");
+
+//determining the winner
+if (randomNumber1 > randomNumber2) {
+  h1.innerHTML = "Player1 Wins!";
+  console.log("Player1 Wins!");
+} else if (randomNumber1 < randomNumber2) {
+  h1.innerHTML = "Player2 Wins!";
+  console.log("Player2 Wins!");
+} else {
+  h1.innerHTML = "Draw!";
+  console.log("Draw!");
 }
